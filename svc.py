774af100c14d0f0ce890cb4cd050e149219ac2e6
Alexsandro <alexsandro.oliveira117@gmail.com>
2020-03-03 00:55:18 -0400

    Add pkg

--- svc.py
+++ svc.py
@@ -6,6 +6,153 @@ import time
 import pathlib
 from tkinter import messagebox
 
+data = {}
+data['temp'] = {
+    "id":True,
+    "pass":True,
+    "model":False,
+    "os":False,
+    "binary":False,
+    "csc":False,
+    "carriers":False,
+    "accounts":False,
+    "issues":False
+}
+data['pkgs']=[{
+    "pkg": "com.samsung.android.mdecservice", 
+    "name": "cmc", 
+    "scope": {
+        "teams": ["settings"], 
+        "inscope": ["call", "message","all"]
+        }
+    },
+    {
+        "pkg":"com.samsung.android.dialer",
+        "name":"Phone",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["call","sanity","all"]
+        }
+    },
+    {
+        "pkg":"com.santander.app",
+        "name":"Santander",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["3rdparty_settings","all"]
+        }
+    },
+    {
+        "pkg":"com.bradesco",
+        "name":"Bradesco",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["3rdparty_settings","all"]
+        }
+    },
+    {
+        "pkg":"br.com.bb.android",
+        "name":"Banco do Brasil",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["3rdparty_settings","all"]
+        }
+    },
+    {
+        "pkg":"br.com.gabba.Caixa",
+        "name":"Caixa",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["3rdparty_settings","all"]
+        }
+    },
+    {
+        "pkg":"com.itau",
+        "name":"Itau",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["3rdparty_settings","all"]
+        }
+    },
+    {
+        "pkg":"com.samsung.android.messaging",
+        "name":"Message",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["message","sanity","all"]
+        }
+    },
+    {
+        "pkg":"com.sec.android.inputmethod",
+        "name":"Keyboard",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["keyboard","keypad","sanity","all"]
+        }
+    },
+    {
+        "pkg":"com.android.settings",
+        "name":"Settings",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["settings","sanity","all"]
+        }
+    },
+    {
+        "pkg":"com.microsoft.appmanager",
+        "name":"Link To Windows",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["message","all"]
+        }
+    },
+    {
+        "pkg":"com.samsung.android.app.telephonyui",
+        "name":"Call Service",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["call","all"]
+        }
+    },
+    {
+        "pkg":"com.samsung.android.app.tips",
+        "name":"Tips",
+        "scope":{
+            "teams":["settings"],
+            "inscope":["settings","all"]
+        }
+    },
+    {
+        "pkg":"com.facebook.katana",
+        "name":"Facebook",
+        "scope":{
+            "teams":["ic"],
+            "inscope":["dual_mensager","all"]
+        }
+    },
+    {
+        "pkg":"com.whatsapp",
+        "name":"Whatsapp",
+        "scope":{
+            "teams":["ic"],
+            "inscope":["dual_mensager","all"]
+        }
+    },
+    
+]
+
+def addPkg(name,pkg,*teams,*scopes):
+    x = {
+        "pkg":pkg,
+        "name":name,
+        "scope":{
+            "teams":teams,
+            "inscope":teams
+        }
+    }
+    data['pkgs'].append()
+
+
 def setADB():#Checar no win adb tem q ta no folder 
     os.putenv('PATH',str(pathlib.Path().absolute()))
 
@@ -96,143 +243,8 @@ def getAppVersion(pName,Name):
             return Name+': '+version+'\n'
         else:
             return ''
-    
 
 def createDefaultConfigFile():
-    data = {}
-    data['temp'] = {
-        "id":True,
-        "pass":True,
-        "model":False,
-        "os":False,
-        "binary":False,
-        "csc":False,
-        "carriers":False,
-        "accounts":False,
-        "issues":False
-    }
-    data['pkgs']=[{
-        "pkg": "com.samsung.android.mdecservice", 
-        "name": "cmc", 
-        "scope": {
-            "teams": ["settings"], 
-            "inscope": ["call", "message","all"]
-            }
-        },
-        {
-            "pkg":"com.samsung.android.dialer",
-            "name":"Phone",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["call","sanity","all"]
-            }
-        },
-        {
-            "pkg":"com.santander.app",
-            "name":"Santander",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["3rdparty_settings","all"]
-            }
-        },
-        {
-            "pkg":"com.bradesco",
-            "name":"Bradesco",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["3rdparty_settings","all"]
-            }
-        },
-        {
-            "pkg":"br.com.bb.android",
-            "name":"Banco do Brasil",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["3rdparty_settings","all"]
-            }
-        },
-        {
-            "pkg":"br.com.gabba.Caixa",
-            "name":"Caixa",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["3rdparty_settings","all"]
-            }
-        },
-        {
-            "pkg":"com.itau",
-            "name":"Itau",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["3rdparty_settings","all"]
-            }
-        },
-        {
-            "pkg":"com.samsung.android.messaging",
-            "name":"Message",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["message","sanity","all"]
-            }
-        },
-        {
-            "pkg":"com.sec.android.inputmethod",
-            "name":"Keyboard",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["keyboard","keypad","sanity","all"]
-            }
-        },
-        {
-            "pkg":"com.android.settings",
-            "name":"Settings",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["settings","sanity","all"]
-            }
-        },
-        {
-            "pkg":"com.microsoft.appmanager",
-            "name":"Link To Windows",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["message","all"]
-            }
-        },
-        {
-            "pkg":"com.samsung.android.app.telephonyui",
-            "name":"Call Service",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["call","all"]
-            }
-        },
-        {
-            "pkg":"com.samsung.android.app.tips",
-            "name":"Tips",
-            "scope":{
-                "teams":["settings"],
-                "inscope":["settings","all"]
-            }
-        },
-        {
-            "pkg":"com.facebook.katana",
-            "name":"Facebook",
-            "scope":{
-                "teams":["ic"],
-                "inscope":["dual_mensager","all"]
-            }
-        },
-        {
-            "pkg":"com.whatsapp",
-            "name":"Whatsapp",
-            "scope":{
-                "teams":["ic"],
-                "inscope":["dual_mensager","all"]
-            }
-        },
-        
-    ]
     with open('config.json','w') as config:
          json.dump(data,config)
     return data
